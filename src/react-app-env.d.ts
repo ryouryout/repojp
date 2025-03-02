3962cf52611a8067d7b3ef4bca4903b5b604cf9c
高橋崚 <takahashiryo@example.com>
2025-03-03 02:31:08 +0900

    不要なファイルを削除

--- src/react-app-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react-scripts" />
-
-// モジュール宣言
-declare module 'react';
-declare module 'react-router-dom';
-declare module '@mui/material';
-declare module '@mui/icons-material';
-declare module 'framer-motion'; 
